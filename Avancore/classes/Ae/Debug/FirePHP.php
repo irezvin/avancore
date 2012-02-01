834226d12d39292a4925639704fa8bf96e7d585c
irezvin <irezvin@a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb>
2012-02-01 12:21:56 +0000

    Changed reference from Ae_Dispatcher to Ae_Avancore in FirePHP.php (otherwise Ae_Controller_Std_Web wasn't able to work)
    git-svn-id: https://avancore.googlecode.com/svn/trunk@4 a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb

--- Avancore/classes/Ae/Debug/FirePHP.php
+++ Avancore/classes/Ae/Debug/FirePHP.php
@@ -7,8 +7,8 @@ class Ae_Debug_FirePHP {
      */
     static function getInstance() {
         if (!class_exists('FirePHP', false)) {
-            require(Ae_Dispatcher::getInstance()->getVendorDir().'/FirePHPCore/FirePHP.class.php');
-            if (!(defined('_DEPLOY_USE_FIRE_PHP') && _DEPLOY_USE_FIRE_PHP || Ae_Dispatcher::getInstance()->config->getValue('useFirePHP', 0)))
+            require(Ae_Avancore::getInstance()->getAdapter()->getVendorPath().'/FirePHPCore/FirePHP.class.php');
+            if (!(defined('_DEPLOY_USE_FIRE_PHP') && _DEPLOY_USE_FIRE_PHP || Ae_Avancore::getInstance()->getAdapter()->getConfigValue('useFirePHP', 0)))
                 FirePHP::getInstance(true)->setEnabled(false);
         }
         return FirePHP::getInstance(true);
