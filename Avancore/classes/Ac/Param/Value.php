29d030fce0439623b280e267dcd6db9f12634c9f
irezvin@gmail.com <irezvin@gmail.com@a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb>
2012-07-27 21:59:24 +0000

    + Added Ac_Param_Value * Work-in-progress on Ac_Cr_Controller * Tests: index.php can now run several tests by specifying comma-separated class list
    git-svn-id: https://avancore.googlecode.com/svn/trunk@21 a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb

---
+++ Avancore/classes/Ac/Param/Value.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * @TODO There is a limitation on Ac_Accessor::getObjectPropertyByPath because it cannot through the arrays
+ * (same for Ac_Util::getArrayByPath which cannot pass through the objects)
+ */
+class Ac_Param_Value {
+    
+    protected $src = false;
+    
+    protected $path = false;
+    
+    protected $default = null;
+    
+    protected $gotValue = false;
+    
+    protected $value = false;
+    
+    protected $cache = false;
+    
+    function __construct($src, $path, $default = null, $cache = false) {
+        if (!(is_array($src) || is_object($src))) throw Ac_E_InvalidCall::wrongType('src', $src, array('array', 'object'));
+        $this->src = $src;
+        $this->path = is_array($path)? $path : Ac_Util::pathToArray($path);
+        $this->default = $default;
+        if ($cache) {
+            if ($this->path) $this->cache = $cache;
+            else throw new Ac_E_InvalidCall("Cannot \$cache with empty \$path");
+        }
+        
+    }
+    
+    /**
+     * This returns src only for non-cached values or for cached values BEFORE their first getValue() call
+     */
+    function getSrc() {
+        return $this->src;
+    }
+    
+    function getDefault() {
+        return $this->default;
+    }
+    
+    function getCache() {
+        return $this->cache;
+    }
+    
+    function setCache($value) {
+        $value = (bool) $value;
+        if ($this->cache && $this->gotValue && !$value) throw new Ac_E_InvalidUsage("Cannot setCache(false) after already called getValue() with enabled \$cache");
+        if ($value && !$this->path) {
+            throw new Ac_E_InvalidCall("Cannot \$cache with empty \$path");
+        }
+    }
+    
+    /**
+     * Short-form alias for getValue()
+     * @return type 
+     */
+    function value() {
+        return $this->getValue();
+    }
+    
+    function getValue() {
+        if ($this->gotValue === false) {
+            $src = $this->src;
+            if (is_object($src) && $src instanceof Ac_Param_Value) $src = $src->getValue();
+            
+            if ($this->path) {
+                if (is_array($src)) $res = Ac_Util::getArrayByPath($src, $this->path, $this->default);
+                elseif (is_object($src)) $res = Ac_Accessor::getObjectPropertyByPath($src, $this->path, $this->default);
+                else $res = $this->default;
+            } else {
+                $res = $this->src;
+            }
+            
+            if ($this->cache) {
+                $this->src = null; // We don't need it anymore so lets free up some memory
+                $this->gotValue = true;
+                $this->value = $res;
+            }
+        } else {
+            $res = $this->value;
+        }
+        return $res;
+    }
+    
+    function __toString() {
+        return htmlspecialchars(''.$this->getValue(), ENT_QUOTES, 'UTF-8');
+    }
+    
+    /**
+     * @param mixed $default
+     * @return Ac_Param_Value 
+     */
+    function cache() {
+        $this->setCache(true);
+        return $this;
+    }
+    
+    /**
+     * @param mixed $default
+     * @return Ac_Param_Value 
+     */
+    function def($default) {
+        $this->default = $default;
+        return $this;
+    }
+    
+    /**
+     * @param string $path
+     * @return Ac_Param_Value 
+     */
+    function __get($path) {
+        $res = new Ac_Param_Value($this, $path, $this->cache);
+        return $res;
+    }
+    
+    /**
+     * Static function to start param chains
+     * Creates Param Value with src, but without any paths
+
+     * @return Ac_Param_Value
+     */
+    static function chain($src) {
+        return new Ac_Param_Value($src, array());
+    }
+    
+}
---
+++ Avancore/tests/classes/Ac/Test/Controller.php
@@ -0,0 +1,115 @@
+<?php
+
+class Ac_Test_Controller extends Ac_Test_Base {
+    
+    function testBasic() {
+        
+        $base = 'http://example.com/index.php';
+        $query = '?action=details&id=1';
+        $url = $base.$query;
+        
+        $rq = new Ac_Request();
+        $rq->populate($url, '/index.php');
+        
+        $ctx = new Ac_Cr_Context();
+        $ctx->setRequest($rq); 
+        
+        $c = new testController1();
+        $c->setContext($ctx);
+        
+        $this->assertEqual(
+            $c->getUrl().'', 
+            $base,
+            'Controller doesn\'t have any used params after initialization and its URL equals to base since it doesn\'t have path prefix'
+        );
+        
+        $this->assertEqual($c->getAction(), 'details');
+        
+        $this->assertEqual(
+            $c->getUrl().'', 
+            $base.'?action=details',
+            "Controller remembers 'action' param after getAction()"
+        );
+        
+        $result = $c->getResult();
+        
+        $this->assertEqual(
+            $c->getUrl().'', 
+            $url,
+            'Controller remembers used params after method invocation'
+        );
+        
+        $this->assertIsA(
+            $result,
+            'Ac_Cr_Result',
+            'Ac_Cr_Controller::getResult should return Ac_Cr_Result instance'
+        );
+        
+        $this->assertEqual(
+            get_class($result), 
+            'Ac_Cr_Result', 
+            'Method returns Ac_Cr_Result by default'
+        );
+        
+        $this->assertSame(
+            $c->getResult(), 
+            $result, 
+            'Subsequent calls to getResult() return the same instance as before'
+        );
+        
+        $this->assertEqual(
+            $result->getMethodOutput(), 
+            'Id is: 1',
+            'Result content should be the same as expected and contain passed parameter \'id\' value'
+        );
+            
+        // Clear controller internals
+        $c->reset();
+        
+        $this->assertEqual($c->getUrl().'', $base, "controller' getUrl() reverts to base after reset()");
+        
+        $result2 = $c->getResult();
+        
+        $this->assertTrue ($result !== $result2, 
+            'After Ac_Cr_Controller::reset() controller should return other Result instance'
+        );
+        
+        $this->assertIsA($result->getResponse(), 'Ac_Response');
+        
+        $this->assertEqual(
+            $c->getUrl().'',
+            $url,
+            "controller' getUrl() returns used params after second invocation after reset()"
+        );
+        
+    }    
+}
+
+class testController1 extends Ac_Cr_Controller {
+
+    function getId() {
+        return $this->use->id->value();
+    }
+    
+    function actionList() {
+        echo "<h1>This is a list</h1>";
+    }
+    
+    function actionDetails() {
+        echo "Id is: ".$this->getId();
+        return array(
+            'id' => $this->getId(),
+            'name' => 'Some name',
+            'description' => 'Some description',
+        );
+    }
+    
+    function executeMyUrl() {
+        echo "My url is: ".$this->getUrl();
+    }
+    
+    function defaultHandler() {
+        echo "Default handler; action is:".$this->getAction();
+    }
+
+}
