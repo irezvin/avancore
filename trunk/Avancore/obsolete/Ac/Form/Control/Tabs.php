590bf7fdd8211367ba9ed45e9945a059b81a2611
irezvin <irezvin@a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb>
2013-05-24 16:04:36 +0000

    Changes to admin interface.
    Better Paginator compatibility with Joomla3 (css).
    Default base class for children may be set in Ac_Form_Control_Composite.
    Callback for form field generator can be provided in mapper's 'info params'.

--- trunk/Avancore/obsolete/Ac/Form/Control/Tabs.php
+++ trunk/Avancore/obsolete/Ac/Form/Control/Tabs.php
@@ -9,6 +9,8 @@ class Ac_Form_Control_Tabs extends Ac_Form_Control_Composite {
     
     var $templatePart = 'tabs';
     
+    var $defaultChildClass = 'Ac_Form_Control_Tabs_Sheet';
+    
     function addDisplayChild($child) {
         if (!is_a($child, 'Ac_Form_Control_Tabs_Sheet'))
             trigger_error ("Only Ac_Form_Control_Tabs_Sheet controls are allowed to be inside the Tabs control");
