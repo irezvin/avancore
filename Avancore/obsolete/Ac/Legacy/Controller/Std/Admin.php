2e29dae406da1b6aa19deeaf3d3ad45d45f1924e
irezvin@gmail.com <irezvin@gmail.com@a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb>
2013-06-05 15:36:59 +0000

    Small improvements and bugfixes
    Created class Ac_Util_Php for PHP-to-PHP data export and used it in Cg_Template
    Cg_Frontend: added LABELs for checkboxes
    Removed small bug in obsolete class Ac_Form_Helper
    Created Ac_Admin_Processing_Method
    Ac_Decorator::mapYes now converts 0's to empty strings
    
    
    git-svn-id: https://avancore.googlecode.com/svn/trunk@67 a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb

--- Avancore/obsolete/Ac/Legacy/Controller/Std/Admin.php
+++ Avancore/obsolete/Ac/Legacy/Controller/Std/Admin.php
@@ -83,7 +83,7 @@ class Ac_Legacy_Controller_Std_Admin extends Ac_Legacy_Controller_Std_Web {
                     Ac_Legacy_Output_Joomla15::addHtmlToJoomlaToolbar($manager->toolbarContent);
                 }
             }
-            if (strlen($manager->toolbarHeader)) {
+            if (strlen($manager->toolbarHeader) && class_exists('JToolBarHelper')) {
                 JToolBarHelper::title($manager->toolbarHeader);
             }
             $this->_tplData['manager'] = $manager;
--- Avancore/obsolete/Cg/Template.php
+++ Avancore/obsolete/Cg/Template.php
@@ -106,14 +106,14 @@ class Cg_Template extends Ac_Template {
      * Shows php open tag
      */
     function phpOpen() {
-        echo "<"."?php";
+        return Ac_Util_Php::phpOpen();
     }
     
     /**
      * Shows php closing tag
      */
     function phpClose() {
-        echo "?".">";
+        return Ac_Util_Php::phpClose();
     }
     
     function _replaceIndent($match) {
@@ -121,107 +121,28 @@ class Cg_Template extends Ac_Template {
     }
     
     function export($foo, $return = false, $indent = 0) {
-        if (is_array($foo)) $res = Cg_Template::exportArray($foo, $indent, true, true, true);
-        elseif ($foo === 0) $res = '0';
-        else $res = $this->myVarExport($foo, true);
-        
-        if ($return) return $res; 
-            else echo $res;
-    }
-    
-    function myVarExport($array, $return = false, $lvl=0)
-    {
-        // Common output variables
-        $indent      = '  ';
-        $doublearrow = ' => ';
-        $lineend     = ",\n";
-        $stringdelim = '\'';
-
-        // Check the export isn't a simple string / int
-        if (is_object($array) && is_a($array, 'Cg_Php_Expression')) {
-            $out = $array->getExpression();
-        } elseif (is_string($array)) {
-            $out = $stringdelim . str_replace('\'', '\\\'', str_replace('\\', '\\\\', $array)) . $stringdelim;
-        } elseif (is_int($array) || is_float($array)) {
-            $out = (string)$array;
-        } elseif (is_bool($array)) {
-            $out = $array ? 'true' : 'false';
-        } elseif (is_null($array)) {
-            $out = 'NULL';
-        } elseif (is_resource($array)) {
-            $out = 'resource';
-        } else {
-            // Begin the array export
-            // Start the string
-            $out = "array (\n";
-
-            // Loop through each value in array
-            foreach ($array as $key => $value) {
-                // If the key is a string, delimit it
-                if (is_string($key)) {
-                    $key = str_replace('\'', '\\\'', str_replace('\\', '\\\\', $key));
-                    $key = $stringdelim . $key . $stringdelim;
-                }
-
-                $val = Cg_Template::myVarExport($value, true, $lvl+1);
-
-                // Piece together the line
-                for ($i = 0; $i <= $lvl; $i++)
-                    $out .= $indent;
-                $out .= $key . $doublearrow . $val . $lineend;
-            }
-
-            // End our string
-            for ($i = 0; $i < $lvl; $i++)
-                $out .= $indent;
-            $out .= ")";
-        }
-
-        // Decide method of output
-        if ($return) {
-            return $out;
-        } else {
-            echo $out;
-            return;
-        }
+        return Ac_Util_Php::export($foo, $return, $indent);
     }
     
     /**
      * Returns code for initializing given PHP array
      */
     function exportArray($foo, $indent = 0, $withNumericKeys = false, $oneLine = false, $return = false) {
-        $vx = Cg_Template::myVarExport($foo, 1);
-        $vx = preg_replace("/=> \n([ ]+)array \\(/", "=> array (\\1", $vx);
-        $vx = preg_replace_callback("/\n[ ]+/", array(& $this, '_replaceIndent'), $vx);
-        if ($indent) {
-            $ind = str_repeat(" ", $indent);
-            $vx = preg_replace("/\n/", "\n".$ind, $vx);
-        }
-        if (!$withNumericKeys) $vx = preg_replace ("/(\n[ ]+) \\d+ =>/", "\\1", $vx);
-        if ($oneLine) {
-            $vx = preg_replace("/\n[ ]*/", " ", $vx);
-        }
-        if (!$return) echo $vx; 
-            else return $vx;
-            
+        return Ac_Util_Php::exportArray($foo, $indent, $withNumericKeys, $oneLine, $return);
     }
     
     /**
      * Returns escaped and quoted PHP string
      */
     function str ($string, $return = false) {
-        $res = "'".addcslashes($string, "'")."'";
-        if ($return) return $res; 
-            else echo $res;
+        return Ac_Util_Php::str($string, $return);
     }
     
     /**
      * Returns twice-escaped-and-quoted string (useful for strings in generated PHP code that runs SQL operators) 
      */
     function str2 ($string, $return = false) {
-        $res = addcslashes("'".addcslashes($string, "'")."'", "'");
-        if ($return) return $res; 
-            else echo $res;
+        return Ac_Util_Php::str2($string, $return);
     }
     
     /**
