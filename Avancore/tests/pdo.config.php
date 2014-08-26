26d2a9bf4028e13449b9ed7989d71747d78b5abd
irezvin <irezvin@gmail.com>
2014-08-26 15:10:49 +0000

    Small bugfixes
    * Fixed bug in Ac_Model_Relation::_countSrcOrDest() - wrong countWithValues() invocation;
    * Fixed bugs related to Ac_Legacy_Database_Mysqli

---
+++ Avancore/tests/pdo.config.php
@@ -0,0 +1,11 @@
+<?php
+
+require(dirname(__FILE__).'/app.config.php');
+
+$config = array(
+    'db' => $dbname,
+    'prefix' => $dbprefix,
+    'user' => $dbuser,
+    'password' => $dbpassword,
+);
+
