cce36a8fb1b2dc472d51d28869d3704f2999d577
irezvin@gmail.com <irezvin@gmail.com@a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb>
2014-12-03 17:01:54 +0000

    Flags improvement; added Periodic & Lock - basic versions
    + Ac_Flags now can get/set flag file contents
    + Ac_Util_DeleteOldFiles now accept arrays to setDirName()
    + Ac_Util_Lock to create locks using the files
    + Ac_Util_Periodic to execute some actions periodically
      (use flags to remember last execution time, can use Locks
      to protect long-running processes)
    + getters.php now accept //comments in the params definition
    
    git-svn-id: https://avancore.googlecode.com/svn/trunk@208 a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb

--- Avancore/web/util/getters.php
+++ Avancore/web/util/getters.php
@@ -1,5 +1,7 @@
 <?php
     if (isset($_REQUEST['submit'])) $foc = 'res'; else $foc = 'getters';
+    ini_set('error_reporting', E_ALL);
+    ini_set('display_errors', 1);
 ?>
 <html>
     <head>
@@ -7,23 +9,29 @@
     </head>
     <body onload="document.gform.<?php echo $foc; ?>.focus();">
         <form method='post' name='gform'>
-            <label for='getters'>Pro<u>p</u>s list:</label>
+            <label for='getters'>Prop<u>s</u> list:</label>
             <table cols='2'>
                 <tr><td valign='top'>
-                        <textarea name='getters' cols='30' rows='10' accesskey="p" onfocus="this.select();"><?php if (isset($_REQUEST['getters'])) echo $_REQUEST['getters']; ?></textarea>
+                        <textarea name='getters' cols='80' rows='10' accesskey="s" onfocus="this.select();"><?php if (isset($_REQUEST['getters'])) echo $_REQUEST['getters']; ?></textarea>
                     </td>
-                    <td valign='top'>
-                        <p>Format: propName [: className] [p][s|g][c[f]][l[x]]</p>
-                        <p>"p" = protected setter | "s" = create setter only | "g" = create getter only | "c" = track change in setter | "l" = lazy getter</p>
-                        <p>"f" = add $force param to track-change setter | "x" = load value from $this->_context in lazy getter</p>
+                    <td valign='top' colspan="2">
+                        <p>Format: propName [: className] [p][s|g][c[f]][l[x]] [//comment[\nNew line]]</p>
+                        <ul>
+                            <li>"p" = protected setter</li>
+                            <li>"s" = create setter only</li>
+                            <li>"g" = create getter only</li>
+                            <li>"c" = track change in setter</li>
+                            <li>"l" = lazy getter</li>
+                            <li>"f" = add $force param to track-change setter</li>
+                            <li>"x" = load value from $this->_context in lazy getter</li>
+                        </ul>
                         <p><button name='submit' accesskey="a">Gener<u>a</u>te!</button><p>
                     </td>
                 </tr>
-            </table>
 <?php   if (isset($_REQUEST['submit'])) { $vars = array(); $sg = array();
     
     foreach (explode("\n", $_REQUEST['getters']) as $line) {    
-        if (preg_match ('#(\w+)(\s*:\s*[\\\\\w]+)?(\s*\w+)?#', trim($line), $matches)) {
+        if (preg_match ('#(\w+)(\s*:\s*[\\\\\w]+)?(\s*\w+)?(\s*//.*+)?#', trim($line), $matches)) {
             $varName = $matches[1];
             $setterName = "set".ucfirst($varName);
             $getterName = "get".ucfirst($varName);
@@ -34,6 +42,7 @@
             $lazyGetter = isset($matches[3]) && strpos($matches[3], 'l') !== false;
             $useThisContext = isset($matches[3]) && strpos($matches[3], 'x') !== false;
             $force = isset($matches[3]) && strpos($matches[3], 'f') !== false;
+            $comment = isset($matches[4])? ltrim(trim($matches[4]), '/') : '';
             
             $type = false;
             if (isset($matches[2])) {
@@ -42,27 +51,39 @@
             
             $defaultValue = 'false';
             
+            $tComm = [];
+            $stComm = [];
+            $gComm = [];
+            
+            if (strlen($comment)) {
+                $comment = explode('\n', $comment);
+                
+                $stComm = array_merge($comment, $stComm);
+                $stComm[0] = "Sets ".$stComm[0];
+                
+                $gComm = array_merge($comment, $gComm);
+                $gComm[0] = "Returns ".$gComm[0];
+                
+                $tComm = array_merge($comment, $tComm);
+            }
+            
             if ($type) {
-                $tComm = "    /**\n     * @var {$type}\n     */\n";
+                $tComm[] = "@var {$type}";
                 if (in_array($type, array('bool', 'boolean')) && $lazyGetter) {
                     $defaultValue = 'null';
                 }
                 if (!isSimpleType($type)) {
                     $sType = "{$type} ";
-                    $stComm = "";
                 } else {
                     $sType = "";
-                    $stComm = "/**\n     * @param {$type} \${$varName}\n     */\n    ";
+                    $stComm[] = "@param {$type} \${$varName}";
                 }
-                $gComm = "    /**\n     * @return {$type}\n     */\n";
+                $gComm[] = "@return {$type}";
             } else {
-                $tComm = "";
                 $sType = "";
-                $gComm = "";
-                $stComm = "";
             }
             
-            $vars[] = "{$tComm}    protected \${$varName} = $defaultValue;";
+            $vars[] = getComment($tComm)."    protected \${$varName} = $defaultValue;";
             
             if ($force) {
                 $forceParam = ', $force = false';
@@ -74,12 +95,12 @@
             
             if (!$isGetterOnly) {
                 if (!$trackChange) {
-                    $sg[] = "    {$stComm}".($isProtectedSetter? "protected " : "")."function {$setterName}({$sType}\${$varName}) {
+                    $sg[] = getComment($stComm)."    ".($isProtectedSetter? "protected " : "")."function {$setterName}({$sType}\${$varName}) {
         \$this->{$varName} = \${$varName};
     }";
                 } else {
                     $uVarName = ucFirst($varName);
-                    $sg[] = "    {$stComm}".($isProtectedSetter? "protected " : "")."function {$setterName}({$sType}\${$varName}{$forceParam}) {
+                    $sg[] = getComment($stComm)."    ".($isProtectedSetter? "protected " : "")."function {$setterName}({$sType}\${$varName}{$forceParam}) {
         if (\${$varName} !== (\$old{$uVarName} = \$this->{$varName}){$forceInset}) {
             \$this->{$varName} = \${$varName};
         }
@@ -92,13 +113,13 @@
             \$this->{$varName} = \$this->_context->getData('{$varName}');";
                     else $inset = "";
                 
-                    $sg[] = "{$gComm}    function {$getterName}() {
+                    $sg[] = getComment($gComm)."    function {$getterName}() {
         if (\$this->{$varName} === $defaultValue) {"."$inset
         }
         return \$this->{$varName};
     }";
                 } else {
-                    $sg[] = "{$gComm}    function {$getterName}() {
+                    $sg[] = getComment($gComm)."    function {$getterName}() {
         return \$this->{$varName};
     }";         
                 }
@@ -106,13 +127,16 @@
         }
     }
 ?>
-<label for="res"><u>r</u>esult:</label><br />
-<textarea cols="80" rows="30" name="res" accesskey="r" onfocus="this.select();">
-<?php
-    echo implode("\n\n", $vars)."\n\n".implode("\n\n", $sg);
-?>
-</textarea>
+    <tr>
+        <td>
+            <label for="res"><u>r</u>esult:</label><br />
+            <textarea cols="80" rows="30" name="res" accesskey="r" onfocus="this.select();"><?php
+                echo implode("\n\n", $vars)."\n\n".implode("\n\n", $sg);
+            ?></textarea>
+        </td>
+    </tr>
 <?php   } ?>
+    </table>
 </form>
 </html>
             
@@ -121,4 +145,14 @@
 
     function isSimpleType($foo) {
         return in_array(trim($foo), array('bool', 'boolean', 'string',  'double', 'float', 'int', 'integer'));
+    }
+    
+    function getComment(array $comm) {
+        if (count($comm)) {
+            foreach ($comm as & $l)  $l = "\n     * {$l}";
+            $res = "    /**".implode("", $comm)."\n     */\n";
+        } else {
+            $res = "";
+        }
+        return $res;
     }
