8bd2028ddef0be2ed5a850239d9c21ff988d4c99
irezvin@gmail.com <irezvin@gmail.com@a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb>
2015-01-31 21:09:16 +0000

    Reverted Ac_Cg_Util::findCommonPrefixes()
    git-svn-id: https://avancore.googlecode.com/svn/trunk@216 a861b35c-4bf2-2bae-69b3-8ea7ccb90bbb

--- Avancore/classes/Ac/Cg/Util.php
+++ Avancore/classes/Ac/Cg/Util.php
@@ -121,24 +121,16 @@ abstract class Ac_Cg_Util {
             do {
                 $match = true;
                 $s = false;
-                $same = 0;
                 foreach ($strings as $string) {
                     $s1 = substr($string, 0, strlen($res) + 1);
                     if ($s === false) $s = $s1;
-                    elseif ($string === $res) {
-                        $same++;
-                    }
                     elseif ($s != $s1) {
                         $match = false;
+                        break;
                     }
                 }
-                if ($same && $same == count($strings)) { // all the strings are same
-                    $match = false;
-                    $res = '';
-                    break;
-                }
                 if ($match) $res = $s;
-            } while ($match && strlen($res));
+            } while ($match);
         }
         return $res;
     }
