64154d7bbec3411a844f63992e42328b84af9e3d
irezvin <irezvin@gmail.com>
2015-03-29 22:47:35 +0000

    Ac_Model_Association improvements
    +   Model & mapper method guessing
    +   Proper model & mapper set-up from Ac_Cg
    +    support (cleared after cloning)
    +   Ac_Model_Object: ->getMapper()
        replaced with ->mapper since mappers are
        always provided
    +

--- Avancore/classes/Ac/Util.php
+++ Avancore/classes/Ac/Util.php
@@ -346,8 +346,9 @@ abstract class Ac_Util {
         return $res;
     }
     
-    static function getObjectProperty($item, $propertyName, $defaultValue = null, $treatArraysAsObjects = false) {
-        return Ac_Accessor::getObjectProperty($item, $propertyName, $defaultValue, $treatArraysAsObjects);
+    static function getObjectProperty($object, $property, $defaultValue = false, $treatArraysAsObjects = false) {
+        $res = Ac_Accessor::getObjectProperty($item, $propertyName, $defaultValue, $treatArraysAsObjects);
+        return $res;
     }
 
     static function stripSlashes($value ) {
