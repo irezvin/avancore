c83543e7a827fda133e185f69c4ccf2c12850f8a
Ilya Rezvin <irezvin@gmail.com>
2016-09-14 23:48:35 +0300

    Removed Ac_Prototype_Builder, Ac_Prototype_Chain (didn't use)

--- Avancore/classes/Ac/Avancore.php
+++ Avancore/classes/Ac/Avancore.php
@@ -8,7 +8,7 @@ if (!class_exists('Ac_Util', false)) {
 
 class Ac_Avancore extends Ac_Application {
     
-    const version = '0.3.4a';
+    const version = '0.3.4-dev';
     
     protected $defaultAssetsPlaceholder = '{AC}';
     
