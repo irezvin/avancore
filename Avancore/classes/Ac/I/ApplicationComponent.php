639be433a58f93850c10dc87cc0197c70f489108
Ilya Rezvin <irezvin@gmail.com>
2019-01-26 16:23:42 +0200

    New interfaces: ApplicationComponent, Controller, NamedApplicationComponent

---
+++ Avancore/classes/Ac/I/ApplicationComponent.php
@@ -0,0 +1,9 @@
+<?php
+
+interface Ac_I_ApplicationComponent {
+    
+    function setApplication(Ac_Application $application);
+    
+    function getApplication();
+    
+}
---
+++ Avancore/classes/Ac/I/NamedApplicationComponent.php
@@ -0,0 +1,9 @@
+<?php
+
+interface Ac_I_NamedApplicationComponent extends Ac_I_ApplicationComponent {
+    
+    function setId($id);
+    
+    function getId();
+    
+}
